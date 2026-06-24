0fec6add9799bbc5f7963dfc997fab95733095f6
yeris01 <hernandezeleanyeris@gmail.com>
2026-06-24 08:49:52 -0500

    Datos dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.db.models import Count
+from django.db.models import Count, Avg
 from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -10,7 +11,11 @@ from etl.models import Paciente, LogETL
 from etl.serializers import PacienteSerializer
 
 def calcular_kpis():
-    distribucion = dict(Paciente.objects.values_list('riesgo_enfermedad').annotate(total=Count('id')))
+    distribucion = dict(
+        Paciente.objects.values_list('riesgo_enfermedad')
+        .annotate(total=Count('id'))
+    )
+
     return {
         'total_pacientes': Paciente.objects.count(),
         'pacientes_criticos': Paciente.objects.filter(es_critico=True).count(),
@@ -18,9 +23,15 @@ def calcular_kpis():
         'pacientes_diabeticos': Paciente.objects.filter(glucosa__gte=126).count(),
         'pacientes_fumadores': Paciente.objects.filter(fumador=True).count(),
         'pacientes_obesidad': Paciente.objects.filter(imc__gte=30).count(),
+
+        # NUEVOS KPI
+        'edad_promedio': round(Paciente.objects.aggregate(Avg('edad'))['edad__avg'] or 0, 1),
+        'imc_promedio': round(Paciente.objects.aggregate(Avg('imc'))['imc__avg'] or 0, 1),
+        'glucosa_promedio': round(Paciente.objects.aggregate(Avg('glucosa'))['glucosa__avg'] or 0, 1),
+        'presion_promedio': round(Paciente.objects.aggregate(Avg('presion_sistolica'))['presion_sistolica__avg'] or 0, 1),
+
         'distribucion_riesgo': distribucion,
     }
-
 @login_required
 def index(request):
     return render(request, 'dashboard/index.html', {'kpis': calcular_kpis(), 'logs_recientes': LogETL.objects.all()[:5]})
